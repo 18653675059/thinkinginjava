533eb1adc3970db0837a106f66923b9186463692
admin <admin@admin-PC>
2016-11-01 23:39:01 +0800

    第七章练习5

---
+++ src/thinkinginjava/E05_Reusing.java
@@ -0,0 +1,21 @@
+package thinkinginjava;
+class A{
+	A(){
+		System.out.println("A Constructor");
+	}
+}
+
+class B{
+	B(){
+		System.out.println("B Constructor");
+	}
+}
+
+class C extends A{
+	B b = new B();
+}
+public class E05_Reusing {
+	public static void main(String[] args) {
+		new C();
+	}
+}
