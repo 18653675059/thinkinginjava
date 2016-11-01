da0ff344838f6bae8bca7cf09098984f1e188cef
admin <admin@admin-PC>
2016-11-01 23:38:44 +0800

    第七章练习3和4

---
+++ src/thinkinginjava/Cartoon.java
@@ -0,0 +1,25 @@
+package thinkinginjava;
+/**
+ * 第七章练习3和4
+ * 练习4缺少一个前提：当构建子类对象时，基类构造器总是会被调用，并且在子类构造器之前被调用
+ * @author Carlos
+ *
+ */
+class Art{
+	Art(){
+		System.out.println("Art Constructor");
+	}
+}
+
+class Drawing extends Art{
+	Drawing(){
+		System.out.println("Drawing Constructor");
+	}
+}
+
+public class Cartoon extends Drawing{
+
+	public static void main(String[] args){
+		Cartoon x = new Cartoon();
+	}
+}
