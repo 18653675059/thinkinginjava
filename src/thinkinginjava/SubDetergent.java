c5bb95697dadacf603391cce08a8c3250ca6851d
admin <admin@admin-PC>
2016-11-01 22:11:56 +0800

    7.2 练习2

---
+++ src/thinkinginjava/SubDetergent.java
@@ -0,0 +1,16 @@
+package thinkinginjava;
+/**
+ * 7.2练习2
+ * @author Carlos
+ *
+ */
+public class SubDetergent extends Detergent{
+	
+	public void scrub(){
+		super.scrub();
+	}
+	
+	public void sterilize(){
+		
+	}
+}
