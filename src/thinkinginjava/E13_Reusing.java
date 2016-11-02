10e7d141f61c7c7cb6e6bdf603ea1ea3cca77ccb
admin <admin@admin-PC>
2016-11-02 23:22:32 +0800

    第七章练习13

---
+++ src/thinkinginjava/E13_Reusing.java
@@ -0,0 +1,31 @@
+package thinkinginjava;
+class Homer{
+
+	void doh(int i){
+		System.out.println("int");
+	}
+	
+	void doh(String a){
+		System.out.println("String");
+	}
+	
+	void doh(char c){
+		System.out.println("char");
+	}
+}
+
+class Bart extends Homer{
+
+	void doh(Double d){
+		System.out.println("double");
+	}
+}
+
+public class E13_Reusing {
+	public static void main(String[] args) {
+		new Bart().doh(1);
+		new Bart().doh(1.2);
+		new Bart().doh('b');
+		new Bart().doh("111111");
+	}
+}
