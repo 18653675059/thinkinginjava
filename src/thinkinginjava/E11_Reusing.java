d8a584b7b5ce018911f31f46a5a298bdbba5b328
admin <admin@admin-PC>
2016-11-02 23:21:51 +0800

    第七章练习11

---
+++ src/thinkinginjava/E11_Reusing.java
@@ -0,0 +1,55 @@
+package thinkinginjava;
+class Cleanser{
+	private String s = "Cleanser";
+	
+	public void append(String a){
+		s += a;
+	}
+	
+	public void dilute(){
+		append(" dilute() ");
+	}
+	
+	public void apply(){
+		append(" apply() ");
+	}
+	
+	public void scrub(){
+		append(" scrub() ");
+	}
+	
+	public String toString(){
+		return s;
+	}
+}
+
+class DetergentDelegation{
+	Cleanser cl = new Cleanser();
+	
+	public void append(String a){
+		cl.append(a);
+	}
+	
+	public void dilute(){
+		cl.dilute();
+	}
+	
+	public void apply(){
+		cl.apply();
+	}
+	
+	public void scrub(){
+		cl.scrub();
+	}
+	
+	public String toString(){
+		return cl.toString();
+	}
+	
+	public void foam(){
+		append(" foam() ");
+	}
+}
+public class E11_Reusing {
+
+}
